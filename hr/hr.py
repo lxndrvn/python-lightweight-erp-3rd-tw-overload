3cfaa797e9a993d00e079958daab569483d66e50
lxndrvn <i1990522@gmail.com>
2016-11-16 10:32:23 +0100

    alex 1.0

--- hr/hr.py
+++ hr/hr.py
@@ -31,9 +31,9 @@ def start_module():
     elif options == 4:
         update("person.csv", id)
     elif options == 5:
-        get_oldest_person("persons.csv")
+        get_oldest_person(table)
     elif options == 6:
-        get_persons_closest_to_average("person.csv")
+        get_persons_closest_to_average(table)
 
 # print the default table of records from the file
 #
@@ -41,9 +41,9 @@ def start_module():
 
 
 def show_table(table):
-    list_of_titles = ["Data Structure", "ID", "Name", "Birth Date"]
+    list_of_titles = ["ID", "Name", "Birth Date"]
     ui.print_table(data_manager.get_table_from_file(table), list_of_titles)
-
+    start_module()
 
 # data structure:
 # id: string
@@ -54,22 +54,30 @@ def show_table(table):
 # Ask a new record as an input from the user than add it to @table, than return @table
 #
 # @table: list of lists
+
+
 def add(table):
-
-        # your code
-
-    return table
-
+    list_of_titles = ["ID", "Name", "Birth date"]
+    table_to_extend = data_manager.get_table_from_file(table)
+    table_to_extend.append(common.ask_for_data_to_add(table_to_extend, list_of_titles[1:]))
+    data_manager.write_table_to_file('persons.csv', table_to_extend)
+    start_module()
 
 # Remove the record having the id @id_ from the @list, than return @table
 #
 # @table: list of lists
 # @id_: string
+
+
 def remove(table, id_):
 
-    # your code
-
-    return table
+    table_to_shorten = data_manager.get_table_from_file(table)
+    try:
+        table_to_shorten.remove(common.ask_for_data_to_remove(table_to_shorten, id_))
+        data_manager.write_table_to_file('persons.csv', table_to_shorten)
+    except:
+        ui.print_error_message("No person found with this ID!\n")
+    start_module()
 
 
 # Update the record in @table having the id @id_ by asking the new data from the user,
@@ -78,28 +86,44 @@ def remove(table, id_):
 # @table: list of lists
 # @id_: string
 def update(table, id_):
-
-    # your code
-
-    return table
-
+    list_of_titles = ["ID", "Name", "Birth date"]
+    table_to_update = data_manager.get_table_from_file(table)
+    updated_table = common.ask_for_data_and_update(table_to_update, id_, list_of_titles[1:])
+    data_manager.write_table_to_file('persons.csv', updated_table)
+    start_module()
 
 # special functions:
 # ------------------
 
 # the question: Who is the oldest person ?
 # return type: list of strings (name or names if there are two more with the same value)
+
+
 def get_oldest_person(table):
-
-    # your code
-
-    pass
+    oldest_year = table[0][2]
+    oldest = [table[0][1]]
+    for i in range(len(table)):
+        if int(table[i][2]) < int(oldest_year):
+            oldest_year = table[i][2]
+            oldest = [table[i][1]]
+        elif int(table[i][2]) == int(oldest_year):
+            oldest.append(table[i][1])
+    return oldest
 
 
 # the question: Who is the closest to the average age ?
 # return type: list of strings (name or names if there are two more with the same value)
 def get_persons_closest_to_average(table):
-
-    # your code
-
-    pass
+    sum_year = 0
+    for i in range(len(table)):
+        sum_year = sum_year + int(table[i][2])
+    average = int(sum_year / len(table))
+    smallest_difference = abs(average - int(table[0][2]))
+    closest_person = [table[0][1]]
+    for i in range(len(table)):
+        if abs(average - int(table[i][2])) < int(smallest_difference):
+            closest_person = [table[i][1]]
+            smallest_difference = abs(average - int(table[i][2]))
+        elif abs(average - int(table[i][2])) == int(smallest_difference):
+            closest_person.append(table[i][1])
+    return closest_person
--- store/store.py
+++ store/store.py
@@ -33,70 +33,90 @@ def start_module():
     elif options == 4:
         update("games.csv", id)
     elif options == 5:
-        get_counts_by_manufacturers("games.csv")
+        get_counts_by_manufacturers(table)
     elif options == 6:
-        get_average_by_manufacturer("games.csv", manufacturer)
+        get_average_by_manufacturer(table, manufacturer)
 
+# else?
 
 # print the default table of records from the file
 #
 # @table: list of lists
+
+
 def show_table(table):
-
-    # your code
-
-    pass
-
+    list_of_titles = ["ID", "Title", "Manufacturers", "Price", "in stock"]
+    ui.print_table(data_manager.get_table_from_file(table), list_of_titles)
+    start_module()
 
 # Ask a new record as an input from the user than add it to @table, than return @table
 #
 # @table: list of lists
+
+
 def add(table):
-
-    # your code
-
-    return table
-
+    list_of_titles = ["ID", "Title", "Manufacturers", "Price", "in stock"]
+    table_to_extend = data_manager.get_table_from_file(table)
+    table_to_extend.append(common.ask_for_data_to_add(table_to_extend, list_of_titles[1:]))
+    data_manager.write_table_to_file('games.csv', table_to_extend)
+    start_module()
 
 # Remove the record having the id @id_ from the @list, than return @table
 #
 # @table: list of lists
 # @id_: string
+
+
 def remove(table, id_):
-
-    # your code
-
-    return table
-
+    table_to_shorten = data_manager.get_table_from_file(table)
+    try:
+        table_to_shorten.remove(common.ask_for_data_to_remove(table_to_shorten, id_))
+        data_manager.write_table_to_file('games.csv', table_to_shorten)
+    except:
+        ui.print_error_message("No game found with this ID!\n")
+    start_module()
 
 # Update the record in @table having the id @id_ by asking the new data from the user,
 # than return @table
 #
 # @table: list of lists
 # @id_: string
+
+
 def update(table, id_):
-
-    # your code
-
-    return table
-
+    list_of_titles = ["ID", "Title", "Manufacturers", "Price", "in stock"]
+    table_to_update = data_manager.get_table_from_file(table)
+    updated_table = common.ask_for_data_and_update(table_to_update, id_, list_of_titles[1:])
+    data_manager.write_table_to_file('games.csv', updated_table)
+    start_module()
 
 # special functions:
 # ------------------
 
 # the question: How many different kinds of game are available of each manufacturer?
 # return type: a dictionary with this structure: { [manufacturer] : [count] }
+
+
 def get_counts_by_manufacturers(table):
-
-    # your code
-
-    pass
-
+    data = {}
+    for i in range(len(table)):
+        stat = table[i][2] in data
+        if stat is not True:
+            data[table[i][2]] = 1
+        else:
+            data[table[i][2]] += 1
+    return data
 
 # the question: What is the average amount of games in stock of a given manufacturer?
 # return type: number
+
+
 def get_average_by_manufacturer(table, manufacturer):
-
-    # your code
-
-    pass
+    all_stock = 0
+    count = 0
+    for i in range(len(table)):
+        if manufacturer == table[i][2]:
+            all_stock += int(table[i][4])
+            count += 1
+    res = all_stock / count
+    return res
