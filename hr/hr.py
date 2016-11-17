9fb37e70835fefd12541d12aea40736c903cd325
lxndrvn <i1990522@gmail.com>
2016-11-17 10:27:29 +0100

    fanifix

--- hr/hr.py
+++ hr/hr.py
@@ -25,17 +25,17 @@ def start():
     table = data_manager.get_table_from_file(current_file_path + "/persons_test.csv")
     inputs = ui.get_inputs(["Please enter a number: "], "")
     option = inputs[0]
-    i_d = 0
+    id = 0
     if option == 1:
         show_table("persons.csv")
     elif option == 2:
         add("persons.csv")
     elif option == 3:
-        i_d = ui.get_inputs(["Enter the ID: "], "")[0])
-        remove("persons.csv", id)
+        id_ = ui.get_inputs(["Enter an ID: "], "")
+        remove("persons.csv", id_)
     elif option == 4:
-        i_d=ui.get_inputs(["Enter the ID: "], "")[0])
-        update("person.csv", id)
+        id_ = ui.get_inputs(["Enter an ID: "], "")[0]
+        update("person.csv", id_)
     elif option == 5:
         get_oldest_person(table)
     elif option == 6:
@@ -52,7 +52,7 @@ def start():
 
 
 def show_table(table):
-    list_of_titles=["ID", "Name", "Birth Date"]
+    list_of_titles = ["ID", "Name", "Birth Date"]
     ui.print_table(data_manager.get_table_from_file(table), list_of_titles)
     return
 
@@ -68,8 +68,8 @@ def show_table(table):
 
 
 def add(table):
-    title_list=["ID", "Name", "Birth Date"]
-    table_to_extend=data_manager.get_table_from_file(table)
+    title_list = ["ID", "Name", "Birth Date"]
+    table_to_extend = data_manager.get_table_from_file(table)
 
     data_manager.write_table_to_file('persons.csv', table_to_extend)
     return
@@ -82,7 +82,7 @@ def add(table):
 
 
 def remove(table, id):
-    remove_data=data_manager.get_table_from_file(table)
+    remove_data = data_manager.get_table_from_file(table)
     # to_remove =
     data_manager.write_table_to_file(current_file_path + "/persons.csv", table)
     return
@@ -95,8 +95,8 @@ def remove(table, id):
 # @id_: string
 
 def update(table, id):
-    title_list=["ID", "Name", "Birth Date"]
-    update_table=data_manager.get_table_from_file(table)
+    title_list = ["ID", "Name", "Birth Date"]
+    update_table = data_manager.get_table_from_file(table)
     # to_update = common?
     data_manager.write_table_to_file(current_file_path + "/persons.csv", table)
     return
@@ -109,12 +109,12 @@ def update(table, id):
 
 
 def get_oldest_person(table):
-    oldest_year=table[0][2]
-    oldest=[table[0][1]]
+    oldest_year = table[0][2]
+    oldest = [table[0][1]]
     for i in range(len(table)):
         if int(table[i][2]) < int(oldest_year):
-            oldest_year=table[i][2]
-            oldest=[table[i][1]]
+            oldest_year = table[i][2]
+            oldest = [table[i][1]]
         elif int(table[i][2]) == int(oldest_year):
             oldest.append(table[i][1])
     return oldest
@@ -124,16 +124,16 @@ def get_oldest_person(table):
 # return type: list of strings (name or names if there are two more with the same value)
 
 def get_persons_closest_to_average(table):
-    sum_year=0
+    sum_year = 0
     for i in range(len(table)):
-        sum_year=sum_year + int(table[i][2])
-    average=int(sum_year / len(table))
-    smallest_difference=abs(average - int(table[0][2]))
-    closest_person=[table[0][1]]
+        sum_year = sum_year + int(table[i][2])
+    average = int(sum_year / len(table))
+    smallest_difference = abs(average - int(table[0][2]))
+    closest_person = [table[0][1]]
     for i in range(len(table)):
         if abs(average - int(table[i][2])) < int(smallest_difference):
-            closest_person=[table[i][1]]
-            smallest_difference=abs(average - int(table[i][2]))
+            closest_person = [table[i][1]]
+            smallest_difference = abs(average - int(table[i][2]))
         elif abs(average - int(table[i][2])) == int(smallest_difference):
             closest_person.append(table[i][1])
     return closest_person
